c78db86b362c3eca98f3027a12cae648691faa5b
Nattapon Phumutta <47663338+phumutta@users.noreply.github.com>
2020-12-14 09:46:44 +0700

    check ls

--- vars/GitPull.groovy
+++ vars/GitPull.groovy
@@ -1,9 +1,12 @@
 def call() {
+        sh 'ls'
+
    sh 'Node --version'
     git 'https://github.com/phumutta/react-docker-jenkins.git'  
     echo "========Git Pull=========="
     sh 'ls'
     sh 'cd work'
+    sh 'ls'
     sh 'npm install'
     echo "=========install=========="
                 
